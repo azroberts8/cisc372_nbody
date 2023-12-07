28fe23ee170cdc1b7463c70a2c50a03caec657cb
Andrew Roberts <andrew@azroberts.com>
2023-12-07 17:49:30 -0500

    Removed serial

--- compute.cu
+++ compute.cu
@@ -48,21 +48,21 @@ void compute(){
 	//make an acceleration matrix which is NUMENTITIES squared in size;
 	int i,j,k;
 	//first compute the pairwise accelerations.  Effect is on the first argument.
-	for (i=0;i<NUMENTITIES;i++){
-		for (j=0;j<NUMENTITIES;j++){
-			if (i==j) {
-				FILL_VECTOR(h_accels[i][j],0,0,0);
-			}
-			else{
-				vector3 distance;
-				for (k=0;k<3;k++) distance[k]=hPos[i][k]-hPos[j][k];
-				double magnitude_sq=distance[0]*distance[0]+distance[1]*distance[1]+distance[2]*distance[2];
-				double magnitude=sqrt(magnitude_sq);
-				double accelmag=-1*GRAV_CONSTANT*mass[j]/magnitude_sq;
-				FILL_VECTOR(h_accels[i][j],accelmag*distance[0]/magnitude,accelmag*distance[1]/magnitude,accelmag*distance[2]/magnitude);
-			}
-		}
-	}
+	// for (i=0;i<NUMENTITIES;i++){
+	// 	for (j=0;j<NUMENTITIES;j++){
+	// 		if (i==j) {
+	// 			FILL_VECTOR(h_accels[i][j],0,0,0);
+	// 		}
+	// 		else{
+	// 			vector3 distance;
+	// 			for (k=0;k<3;k++) distance[k]=hPos[i][k]-hPos[j][k];
+	// 			double magnitude_sq=distance[0]*distance[0]+distance[1]*distance[1]+distance[2]*distance[2];
+	// 			double magnitude=sqrt(magnitude_sq);
+	// 			double accelmag=-1*GRAV_CONSTANT*mass[j]/magnitude_sq;
+	// 			FILL_VECTOR(h_accels[i][j],accelmag*distance[0]/magnitude,accelmag*distance[1]/magnitude,accelmag*distance[2]/magnitude);
+	// 		}
+	// 	}
+	// }
 
 	calcAccel<<<1, NUMENTITIES>>>(d_accels, d_hPos, d_mass);
 	cudaDeviceSynchronize();
@@ -83,17 +83,17 @@ void compute(){
 
 
 	//sum up the rows of our matrix to get effect on each entity, then update velocity and position.
-	for (i=0;i<NUMENTITIES;i++){
-		vector3 accel_sum={0,0,0};
-		for (j=0;j<NUMENTITIES;j++){
-			for (k=0;k<3;k++)
-				accel_sum[k]+=h_accels[i][j][k];
-		}
-		//compute the new velocity based on the acceleration and time interval
-		//compute the new position based on the velocity and time interval
-		for (k=0;k<3;k++){
-			hVel[i][k]+=accel_sum[k]*INTERVAL;
-			hPos[i][k]+=hVel[i][k]*INTERVAL;
-		}
-	}
+	// for (i=0;i<NUMENTITIES;i++){
+	// 	vector3 accel_sum={0,0,0};
+	// 	for (j=0;j<NUMENTITIES;j++){
+	// 		for (k=0;k<3;k++)
+	// 			accel_sum[k]+=h_accels[i][j][k];
+	// 	}
+	// 	//compute the new velocity based on the acceleration and time interval
+	// 	//compute the new position based on the velocity and time interval
+	// 	for (k=0;k<3;k++){
+	// 		hVel[i][k]+=accel_sum[k]*INTERVAL;
+	// 		hPos[i][k]+=hVel[i][k]*INTERVAL;
+	// 	}
+	// }
 }
